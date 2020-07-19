9962b37bccca75fe70b553e6b36ce87e153fc997
DanilShapilov <danil.shapilov225@gmail.com>
2020-07-19 11:27:45 +0500

    template for jquery plugin

---
+++ src/components/start.ts
@@ -0,0 +1,27 @@
+(function($) {
+
+	$.fn.PlugInName = function(opts) {
+	
+		// default configuration
+		var config = $.extend({}, {
+			opt1: null
+		}, opts);
+	
+		// main function
+		function DoSomething(e) {
+		}
+
+		// initialize every element
+		this.each(function() {
+			DoSomething($(this));
+		});
+
+		return this;
+	};
+
+	// start
+	$(function() {
+		$("#select").PlugInName();
+	});
+
+})(jQuery);
