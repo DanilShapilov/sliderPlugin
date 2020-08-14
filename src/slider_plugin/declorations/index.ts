77e0feb29f321ccb0a0bc4a3e8bfaf6792ba12bd
DanilShapilov <danil.shapilov225@gmail.com>
2020-08-14 11:34:16 +0500

    more references added

--- src/slider_plugin/declorations/index.ts
+++ src/slider_plugin/declorations/index.ts
@@ -1,5 +1,7 @@
 /// <reference path="SliderPlugin.d.ts" />
 /// <reference path="SliderModel.d.ts" />
+/// <reference path="SliderPresenter.d.ts" />
+/// <reference path="SliderView.d.ts" />
 /// <reference path="ResizeObserver.d.ts" />
 
 interface JQuery{
