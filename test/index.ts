8eb75fc6d9175b220ca34425539488b7fb0e9995
DanilShapilov <39529221+DanilShapilov@users.noreply.github.com>
2020-08-24 17:03:14 +0500

    remove unnecessary code form tests index.ts

--- test/index.ts
+++ test/index.ts
@@ -1,8 +1,7 @@
 // declare var require: any;
-
 const testsContext = require.context(".", true, /\.spec.ts$/);
 testsContext.keys().forEach(testsContext);
 
 // add all ts files to include non referenced files in report
-const srcContext = require.context("../src", true, /^\.\/(?!index(\.ts|\.html)?$)/);
-srcContext.keys().forEach(srcContext);
+// const srcContext = require.context("../src", true, /^\.\/(?!index(\.ts|\.html)?$)/);
+// srcContext.keys().forEach(srcContext);
